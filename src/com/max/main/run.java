e90ab6a298e0a91b51813f0e7d47035e9d15de2b
Max Hwang <maxhwang15@gmail.com>
2022-01-31 13:57:36 +0900

    starting baek question in intelliJ

--- src/com/max/main/run.java
+++ src/com/max/main/run.java
@@ -1,4 +1,9 @@
 package com.max.main;
 
+import com.max.practice.Baek_2557;
+
 public class run {
+    public static void main(String[] args) {
+        new Baek_2557().method1();
+    }
 }
--- src/com/max/practice/baek.java
+++
@@ -1,4 +0,0 @@
-package com.max.practice;
-
-public class baek {
-}
