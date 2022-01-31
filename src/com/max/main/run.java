16c8ccc60845f03489438316067db04254d1953f
Max Hwang <maxhwang15@gmail.com>
2022-01-31 14:14:28 +0900

    method2

--- src/com/max/main/run.java
+++ src/com/max/main/run.java
@@ -2,8 +2,11 @@ package com.max.main;
 
 import com.max.practice.Baek_2557;
 
+import java.io.IOException;
+
 public class run {
-    public static void main(String[] args) {
-        new Baek_2557().method1();
+    public static void main(String[] args) throws IOException {
+        // new Baek_2557().method1();
+        new Baek_2557().method2();
     }
 }
--- src/com/max/practice/Baek_2557.java
+++ src/com/max/practice/Baek_2557.java
@@ -1,7 +1,19 @@
 package com.max.practice;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Baek_2557 {
     public void method1(){
         System.out.println("Hello World!");
+    }
+
+    public void method2() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
     }
 }
