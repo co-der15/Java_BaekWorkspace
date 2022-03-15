f97b35e2cdf20fd06416051f98037f4a9ab7318b
Max Hwang <maxhwang15@gmail.com>
2022-03-15 18:02:52 +0900

    2022.03.15 using switch~case

--- src/com/max/main/run.java
+++ src/com/max/main/run.java
@@ -1,6 +1,6 @@
 package com.max.main;
 
-import com.max.practice.Baek_2908;
+import com.max.practice.Baek_5622;
 
 import java.io.IOException;
 
@@ -42,6 +42,7 @@ public class run {
         // new Baek_1152().method2();
         // new Baek_1152().method3();
         // new Baek_2908().method1();
-        new Baek_2908().method2();
+        // new Baek_2908().method2();
+        new Baek_5622().method1();
     }
 }
